5c37e0664cc4e1cdb5fb93cac340bcbcced31cf8
Mohini Chahal <mohinichahal@gmail.com>
2025-10-28 09:14:02 -0700

    converted HTML to React for home page

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,8 +1,34 @@
-// You can name the function within page.tsx anything you want.
-export default function Home() {
+import React from "react";
+import Navbar from "../components/navbar"; // adjust path if needed
+//import "../globals.css";
+
+export default function HomePage() {
   return (
-		<div>
-			// content will go within here.
-		</div>
-		) 
+    <main>
+      <Navbar />
+
+      <h1>welcome to my website!</h1>
+      <h2>it's nice to meet you</h2>
+
+      <div className="about">
+        <div className="about-image">
+          <img src="/monu-pic.png" alt="a picture of me!" />
+        </div>
+
+        <div className="about-text">
+          <p>
+            My name is Mohini, and I am a freshman at Cal Poly SLO! I am majoring
+            in Computer Science, which I am <em>so</em> excited about. I was born
+            and raised in <strong>Fremont, CA</strong> and some of my favorite
+            things to do are go on nature walks/hikes, try new kinds of food,
+            play poker, or volleyball!
+          </p>
+        </div>
+      </div>
+
+      <footer className="footer">
+        © 2025 mohini's website | All Rights Reserved
+      </footer>
+    </main>
+  );
 }
